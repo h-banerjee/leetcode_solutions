09a7b323205c14f6654babc0763a22fe9f3b4021
h-banerjee <harshbanerjee30@gmail.com>
2025-01-04 15:53:09 +0530

    -FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        for i in range(1,n+1):
+            for i%3==0 && i%5==0:
+                res.append("FizzBuzz")
+            elif i%3==0:
+                res.append("Fizz")
+            elif 1%5==0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+            return res
+            
+        
