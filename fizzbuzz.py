2363c857aa032c83090f883624da3417e3018551
h-banerjee <harshbanerjee30@gmail.com>
2025-01-04 15:54:57 +0530

    -corrected code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,8 @@
 class Solution(object):
     def fizzBuzz(self, n):
+        res=[]
         for i in range(1,n+1):
-            for i%3==0 && i%5==0:
+            if i%3==0 and i%5==0:
                 res.append("FizzBuzz")
             elif i%3==0:
                 res.append("Fizz")
